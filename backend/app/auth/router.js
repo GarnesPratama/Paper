db4fb667bb328b17962e51b1bbf3559c61b2f5de
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2022-06-11 18:44:48 +0700

    auth signUp, signIn progress

--- backend/app/auth/router.js
+++ backend/app/auth/router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require("./controller");
 
 router.post("/signup", authController.signUp);
+router.post("/signIn", authController.signIn);
 
 module.exports = router;
--- backend/app/user/model.js
+++ backend/app/user/model.js
@@ -41,4 +41,9 @@ userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, 12);
 });
 
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
 module.exports = mongoose.model("user", userSchema);
