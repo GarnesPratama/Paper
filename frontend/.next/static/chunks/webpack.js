68f7f4ed412867f7c2225a2be08a726e20dd318d
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2022-06-14 20:28:42 +0700

    add fiks

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc7ad1da7cccbc14"; }
+/******/ 		__webpack_require__.h = function() { return "a2dc64cc0f173114"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/.next/static/webpack/webpack.c7b2a5724d58ce0b.hot-update.js
+++ frontend/.next/static/webpack/webpack.c7b2a5724d58ce0b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c0ffede5c169ce09"; }
+/******/ 	__webpack_require__.h = function() { return "a2dc64cc0f173114"; }
 /******/ }();
 /******/ 
 /******/ }
