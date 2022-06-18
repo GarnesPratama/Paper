cfe241283a0ae1cda1af01114878de5aebbfbd94
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2022-06-18 16:37:15 +0700

    add folder bank

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "692cebe56c804f4a"; }
+/******/ 		__webpack_require__.h = function() { return "1f79e941247d3146"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
