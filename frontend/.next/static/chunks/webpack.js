cdcf830f5becaf74e6a09e5c3ccd49e580c2bfbf
Noor Yulita Apsari <72492009+lita15@users.noreply.github.com>
2022-06-24 10:19:38 +0700

    fix commit

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d9479dbe6f4ec372"; }
+/******/ 		__webpack_require__.h = function() { return "570132aa9c019749"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
