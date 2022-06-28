c99bc0263dde4d15952eeac5cf483e9244ab2756
GarnesPratama <garnesrizky@icloud.com>
2022-06-28 09:09:58 +0700

    Update frontend

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ecb8a3338f6e73d"; }
+/******/ 		__webpack_require__.h = function() { return "8110c91de848457b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
